8f1f254f625a5527306ad1822549292a0065a755
SHUVO <im.computer.engineeer@gamil.com>
2025-07-20 20:29:55 +0600

    Error Page and Dashoboard page added

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,12 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 
-
 const Root = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-        </div>
-    );
+  return (
+    <div>
+      <div className="max-w-[1440px] mx-auto">
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+      </div>
+
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,20 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./Components/Root/Root"; //import from Root jsx file
+import Error from "./Components/Error/Error";
+import Home from "./Components/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+    ],
   },
 ]);
 
